4b01d66b70f5a1bec9e991e09dfc63b7c28f0c3a
Tanya933 <tanya.rajpal0400@gmail.com>
2020-12-26 19:16:00 +0530

    first commit

--- src/Components/AboutUs/AboutUs.js
+++ src/Components/AboutUs/AboutUs.js
@@ -1,6 +1,12 @@
 import React from "react";
-import "./AboutUs";
+import classes from "./AboutUs.module.css";
+import pic from "../../../assets/icons/coffeeCupThought.svg";
 
 export default function AboutUs() {
-  return <div>AboutUS</div>;
+  return (
+    <div className={classes.aboutUs}>
+      <div className={classes.aboutContent}>first box</div>
+      <div className={classes.aboutImg}>{pic}</div>
+    </div>
+  );
 }
