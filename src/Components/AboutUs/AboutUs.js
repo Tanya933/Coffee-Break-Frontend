7b61c2ca7e3b132528f20cddcaa6f4dd4b093037
Tanya933 <tanya.rajpal0400@gmail.com>
2020-12-26 20:12:21 +0530

    Add Basic Route, Navbar, Home, AboutUs Components

--- src/Components/AboutUs/AboutUs.js
+++ src/Components/AboutUs/AboutUs.js
@@ -1,12 +1,6 @@
 import React from "react";
-import classes from "./AboutUs.module.css";
-import pic from "../../../assets/icons/coffeeCupThought.svg";
+import "./AboutUs";
 
 export default function AboutUs() {
-  return (
-    <div className={classes.aboutUs}>
-      <div className={classes.aboutContent}>first box</div>
-      <div className={classes.aboutImg}>{pic}</div>
-    </div>
-  );
+  return <div>AboutUS</div>;
 }
